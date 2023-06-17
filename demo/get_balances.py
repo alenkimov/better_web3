2f1c8b732012cfa87e13bacadeb6c98d093ced81
Alen <alen.kimov@gmail.com>
2023-06-18 01:25:55 +0600

    fix: неверные примеры вывода

--- demo/get_balances.py
+++ demo/get_balances.py
@@ -12,7 +12,6 @@ weth = goerli.erc20("0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6")
 addresses = to_checksum_addresses(
     [
         "0x780afe4a82Ed3B46eA6bA94a1BB8F7b977298722",
-        "0x79c950c7446b234a6ad53b908fbf342b01c4d446",
         "0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6",
     ]
 )
@@ -27,15 +26,13 @@ def print_balances(balances: dict[ChecksumAddress: Wei], token_symbol: str):
 eth_balances_wei = goerli.get_balances(addresses)
 print_balances(eth_balances_wei, "ETH")
 """output:
-[1] [0x780afe4a82Ed3B46eA6bA94a1BB8F7b977298722] 17.98 ETH
-[2] [0x79C950C7446B234a6Ad53B908fBF342b01c4d446] 0.00 ETH
-[3] [0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6] 891130.00 ETH
+[1] [0x780afe4a82Ed3B46eA6bA94a1BB8F7b977298722] 17.72 ETH
+[2] [0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6] 1013680.97 ETH
 """
 
 weth_balances_wei = weth.get_balances(addresses)
 print_balances(weth_balances_wei, weth.get_symbol())
 """output:
-[1] [0x780afe4a82Ed3B46eA6bA94a1BB8F7b977298722] 17.98 ETH
-[2] [0x79C950C7446B234a6Ad53B908fBF342b01c4d446] 0.00 ETH
-[3] [0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6] 891130.00 ETH
+[1] [0x780afe4a82Ed3B46eA6bA94a1BB8F7b977298722] 1.20 WETH
+[2] [0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6] 47.35 WETH
 """
