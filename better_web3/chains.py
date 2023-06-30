8b7af23be08394c36f0da427292f81be0156afa7
Alen <alen.kimov@gmail.com>
2023-07-01 01:21:33 +0600

    feat: v1.2.2
    - Теперь можно задавать параметры сети в функции `load_chains`

--- better_web3/chains.py
+++ better_web3/chains.py
@@ -6,13 +6,13 @@ CHAINS_FILEPATH = SCRIPT_DIR / "chains.toml"
 CHAINS_DATA = load_toml(CHAINS_FILEPATH)
 
 
-def load_chains(chains_data: dict, ensure_chain_id=True) -> dict[int: Chain]:
+def load_chains(chains_data: dict, ensure_chain_id=True, **chain_kwargs) -> dict[int: Chain]:
     chains: dict[int: Chain] = {}
     for net_mode, id_to_chain_data in chains_data.items():
         is_testnet = True if net_mode == "testnet" else False
         for chain_id, chain_data in id_to_chain_data.items():
             chain_id = int(chain_id)
-            chain = Chain(**chain_data, is_testnet=is_testnet)
+            chain = Chain(**chain_data, is_testnet=is_testnet, **chain_kwargs)
             if ensure_chain_id and chain.chain_id == chain_id or not ensure_chain_id:
                 chains[chain_id] = chain
     return chains
