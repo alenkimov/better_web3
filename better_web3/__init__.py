dc2462ebbdb52cdd4b0feff6d76bf73407aa21f4
alenkimov <alen.kimov@gmail.com>
2025-03-17 16:17:33 +0300

    tx_hash_info, tx_receipt_info

--- better_web3/__init__.py
+++ better_web3/__init__.py
@@ -2,6 +2,8 @@ from .chain import Chain
 from .contract import Contract
 from .caip_2 import get_chains, get_chain
 from .utils import sign_message
+from .tx import tx_hash_info
+from .tx import tx_receipt_info
 
 
 __all__ = [
@@ -9,5 +11,7 @@ __all__ = [
     "Contract",
     "get_chains",
     "get_chain",
-    "utils",
+    "sign_message",
+    "tx_hash_info",
+    "tx_receipt_info",
 ]
